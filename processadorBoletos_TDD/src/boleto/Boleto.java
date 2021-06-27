aef1ccbdb06eb17096a5ff80c0eaa2cab95aa2a1
Gleison Lima <gleisonlima@id.uff.br>
2021-06-27 13:02:50 -0300

    Criação dos atributos e métodos das classes Boleto e fatura

--- processadorBoletos_TDD/src/boleto/Boleto.java
+++ processadorBoletos_TDD/src/boleto/Boleto.java
@@ -1,5 +1,41 @@
 package boleto;
 
-public class Boleto {
+import java.util.Date;
 
+public class Boleto {
+	
+	private int codigo;
+	private Date dataVencimento;
+	private double valorPago;
+	
+	public Boleto(int codigo, Date dataVencimento, double valorPago) {
+		super();
+		this.codigo = codigo;
+		this.dataVencimento = dataVencimento;
+		this.valorPago = valorPago;
+	}
+	
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+
+	public Date getDataVencimento() {
+		return dataVencimento;
+	}
+
+	public void setDataVencimento(Date dataVencimento) {
+		this.dataVencimento = dataVencimento;
+	}
+
+	public double getValorPago() {
+		return valorPago;
+	}
+
+	public void setValorPago(double valorPago) {
+		this.valorPago = valorPago;
+	}
 }
--- processadorBoletos_TDD/src/fatura/Fatura.java
+++ processadorBoletos_TDD/src/fatura/Fatura.java
@@ -1,5 +1,50 @@
 package fatura;
 
-public class Fatura {
+import java.util.Date;
 
+public class Fatura {
+	
+	private Date dataVencimento;
+	private double valorTotal;
+	private String nomeCliente;
+	private String statuFatura;
+	
+	
+	public Fatura(String nome, double valor, Date data) {		
+		this.nomeCliente = nome;
+		this.valorTotal = valor;
+		this.dataVencimento= data; 
+	}
+	
+	public Date getDataVencimento() {
+		return dataVencimento;
+	}
+
+	public void setDataVencimento(Date dataVencimento) {
+		this.dataVencimento = dataVencimento;
+	}
+	
+	public double getvalorTotal() {
+		return valorTotal;
+	}
+	
+	public void setvalorTotal(double valor) {
+		this.valorTotal = valor;
+	}
+	
+	public String getnomeCliente() {
+		return nomeCliente;
+	}
+	
+	public void setnomeCliente(String nome) {
+		this.nomeCliente = nome;
+	}
+	
+	public String getstatuFatura() {
+		return statuFatura;
+	}
+	
+	public void setstatuFatura(String statuFatura) {
+		this.statuFatura = statuFatura;
+	}
 }
